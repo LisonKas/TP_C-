b8ce64eba3026a6de138e6596c5741630606e942
Lison <lisonfortin@icloud.com>
2024-03-04 17:37:18 +0100

    reste du src td 1 pas fini

---
+++ TD1/src/fraction.cpp
@@ -0,0 +1,117 @@
+#include "fraction.hpp"
+#include "utils.hpp"
+
+#include <iostream>
+
+// void Fraction::display() {
+//     std::cout << numerator << "/" << denominator;
+// }
+
+std::ostream& operator<<(std::ostream& os, Fraction const& p) {
+    return os << '(' << p.numerator << "/" << p.denominator << ')';
+}
+
+Fraction operator+(Fraction const& f1, Fraction const& f2) {
+    return simplify({
+        f1.numerator * f2.denominator + f2.numerator * f1.denominator,
+        f1.denominator * f2.denominator
+    });
+}
+
+// Fraction add(Fraction const& f1, Fraction const& f2) {
+//     Fraction result {
+//         f1.numerator * f2.denominator + f2.numerator * f1.denominator
+//         f1.denominator * f2.denominator
+//     };
+
+//     return simplify(result);
+// }
+
+
+Fraction operator-(Fraction const& f1, Fraction const& f2) {
+    return simplify({
+        f1.numerator * f2.denominator - f2.numerator * f1.denominator,
+        f1.denominator * f2.denominator
+    });
+}
+
+Fraction operator*(Fraction const& f1, Fraction const& f2) {
+    return simplify({
+        f1.numerator * f2.numerator,
+        f1.denominator * f2.denominator
+    });
+}
+
+Fraction operator/(Fraction const& f1, Fraction const& f2) {
+    return simplify({
+        f1.numerator * f2.denominator,
+        f1.denominator * f2.numerator
+    });
+}
+
+bool operator==(Fraction const& f1, Fraction const& f2){
+    if(f1.numerator!=f2.numerator){
+        return false;
+    }
+    else if(f1.denominator!=f2.denominator){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+bool operator!=(Fraction const& f1, Fraction const& f2){
+    if(f1==f2){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+bool operator<(Fraction const& f1, Fraction const& f2){
+    
+    if(static_cast<float>(f2.numerator)/static_cast<float>(f2.denominator) > static_cast<float>(f1.numerator)/static_cast<float>(f1.denominator)){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool operator>(Fraction const& f1, Fraction const& f2){
+    if(f1<f2){
+        return false;
+    }
+    else if(f1==f2){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+bool operator<=(Fraction const& f1, Fraction const& f2){
+    if(f1==f2){
+        return true;
+    }
+    else if(f1<f2){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+bool operator>=(Fraction const& f1, Fraction const& f2){
+    if(f1==f2){
+        return true;
+    }
+    else if(f1>f2){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
---
+++ TD1/src/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include "fraction.hpp"
+
+int main()
+{
+    Fraction f1 { 3, 4 };
+    Fraction f2 { 1, 2 };
+
+    std::cout << "f1 = ";
+    // f1.display();
+    std::cout << f1;
+    std::cout << " f2 = ";
+    // f2.display();
+    std::cout << f2;
+    std::cout << std::endl;
+
+    Fraction f3 { f1+f2 };
+
+    std::cout << "add(f1, f2) = ";
+    // f3.display(); << std::endl
+    std::cout << f3;
+
+    std::cout << std::endl << "sub(f1, f2) = ";
+    // (f1-f2).display();
+    std::cout << f1-f2;
+
+    std::cout << std::endl << "mul(f1, f2) = ";
+    // (f1*f2).display();
+    std::cout << f1*f2;
+
+    std::cout << std::endl << "div(f1, f2) = ";
+    std::cout << f1/f2;
+    std::cout << std::endl;
+
+    std::cout << std::endl << "f1 == f2 ? ";
+    std::cout << (f1==f2);
+    std::cout << std::endl << "f1 != f2 ? ";
+    std::cout << (f1!=f2);
+    std::cout << std::endl;
+
+    std::cout << std::endl << "f1 == f1 ? ";
+    std::cout << (f1==f1);
+    std::cout << std::endl << "f1 != f1 ? ";
+    std::cout << (f1!=f1);
+    std::cout << std::endl;
+
+    std::cout << std::endl << "add(1/6, 2/6) = ";
+    std::cout << (Fraction{1, 6}+Fraction{2, 6}) << std::endl;
+
+
+    std::cout << std::endl << "f1 < f2 ? ";
+    std::cout << (f1<f2);
+    std::cout << std::endl << "f2 < f1 ? ";
+    std::cout << (f2<f1);
+    std::cout << std::endl << "f1 < f1 ? ";
+    std::cout << (f1<f1);
+    std::cout << std::endl;
+
+
+    std::cout << std::endl << "f1 > f2 ? ";
+    std::cout << (f1>f2);
+    std::cout << std::endl << "f2 > f1 ? ";
+    std::cout << (f2>f1);
+    std::cout << std::endl << "f1 > f1 ? ";
+    std::cout << (f1>f1);
+    std::cout << std::endl;
+
+
+    std::cout << std::endl << "f1 <= f2 ? ";
+    std::cout << (f1<=f2);
+    std::cout << std::endl << "f2 <= f1 ? ";
+    std::cout << (f2<=f1);
+    std::cout << std::endl << "f1 <= f1 ? ";
+    std::cout << (f1<=f1);
+    std::cout << std::endl;
+
+
+    std::cout << std::endl << "f1 >= f2 ? ";
+    std::cout << (f1>=f2);
+    std::cout << std::endl << "f2 >= f1 ? ";
+    std::cout << (f2>=f1);
+    std::cout << std::endl << "f1 >= f1 ? ";
+    std::cout << (f1>=f1);
+    std::cout << std::endl;
+
+    return 0;
+}
