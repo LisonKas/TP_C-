003ae88605235ab7562b22db9965eecf1cff397f
Lison <lisonfortin@icloud.com>
2024-03-18 16:54:47 +0100

    exo 2 et 3 td 2

--- TD2/src/exo1.cpp
+++ TD2/src/exo1.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cstdlib>
+#include "ScopedTimer.hpp"
+
+std::vector<int> generate_random_vector(size_t const size, int const max = 100) {
+    std::vector<int> vec(size);
+    std::generate(vec.begin(), vec.end(), [&max]() { return std::rand() % max;} );
+    return vec;
+}
+
+std::vector<float> generate_random_vector2(size_t const size, int const max = 100) {
+    std::vector<float> vec(size);
+    std::generate(vec.begin(), vec.end(), [&max]() { return std::rand() % max;} );
+    return vec;
+}
+
 bool is_sorted(std::vector<int> const& vec) { return std::is_sorted(vec.begin(), vec.end()); }
 bool is_sorted(std::vector<float> const& vec) { return std::is_sorted(vec.begin(), vec.end()); }
 
@@ -19,7 +34,9 @@ void bubble_sort(std::vector<int> & vec){
 
 // EXO 2
 size_t quick_sort_partition(std::vector<float> & vec, size_t left, size_t right, size_t const pivot){
-    float valeur_pivot = vec[pivot];
+    float const valeur_pivot = vec[pivot];
+
+    size_t first_bigger_element_index {left};
 
     std::swap(vec[pivot], vec[right]);
 
@@ -27,16 +44,24 @@ size_t quick_sort_partition(std::vector<float> & vec, size_t left, size_t right,
         if (vec[i] < valeur_pivot) {
             std::swap(vec[i], vec[left]);
 
-            left++;
+            first_bigger_element_index += 1;
         }
     }
-    std::swap(vec[left], vec[right]);
+    std::swap(vec[first_bigger_element_index], vec[right]);
 
-    return left;
+    return first_bigger_element_index;
 }
 
 void quick_sort(std::vector<float> & vec, size_t const left, size_t const right){
-
+    if(left>=right){
+        return;       
+    }
+    size_t pivot {left + (right - left) /2};
+    pivot = quick_sort_partition(vec, left, right, pivot);
+    if(pivot > 0){
+        quick_sort(vec, left, pivot - 1);
+    }
+    quick_sort(vec, pivot + 1, right); 
 }
 
 void quick_sort(std::vector<float> & vec) {
@@ -44,6 +69,7 @@ void quick_sort(std::vector<float> & vec) {
 }
 
 int main(){
+    //EXO 1
     std::vector<int> array {8,4,5,7,2,1,10,9,6};
     std::vector<float> array2 {8,4,5,7,2,1,10,9,6};
     bubble_sort(array);
@@ -56,6 +82,8 @@ int main(){
     } else {
         std::cout << "Le tableau n'est pas trié" << std::endl;
     }
+
+    //EXO 2
     quick_sort(array2);
     if (is_sorted(array2)) {
         std::cout << "Le tableau array2 est trié" << std::endl;
@@ -66,5 +94,19 @@ int main(){
     } else {
         std::cout << "Le tableau n'est pas trié" << std::endl;
     }
+    
+
+    //EXO 3
+    {
+        ScopedTimer timer("Tri rapide");
+        std::vector<float> data = generate_random_vector2(100);
+        quick_sort(data);
+    }
+    {
+        ScopedTimer timer("Tri à bulles");
+        std::vector<int> tableau = generate_random_vector(100);
+        bubble_sort(tableau);
+    }
+
     return 0;
 }
